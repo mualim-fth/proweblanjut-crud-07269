ec4524dae3597807c23c9d219350a3b7fae88b9c
mualim-fth <ahmdfth26@gmail.com>
2026-03-31 20:46:06 +0700

    feat: tambah proteksi halaman

--- tambah.php
+++ tambah.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+
+// cek apakah user sudah login
+if (!isset($_SESSION["username"])) {
+    header("Location: login.php");
+    exit();
+}
+
 include "config/koneksi.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
